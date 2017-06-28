daacb2ed054ef0aa20a5f859bdee1999075f4b3f
william-eyre <william.eyre@sainsburys.co.uk>
2017-06-28 10:52:55 +0100

    working version of the app

--- app/src/main/java/com/williameyre/imdbapplication/MainActivity.java
+++ app/src/main/java/com/williameyre/imdbapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.williameyre.imdbapplication;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +34,13 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Request Sent", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                ImdbJsonDump jsonDump = new ImdbJsonDump();
+                jsonDump.execute();
             }
         });
+        text = (TextView) findViewById(R.id.text);
     }
 
     @Override
@@ -48,5 +63,53 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class ImdbJsonDump extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+
+            BufferedReader reader = null;
+            String link = "http://172.24.79.182:8080/ImdbConnect?content=the+Dark+Knight";
+
+            try {
+                URL url = new URL(link);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true);
+                urlConnection.connect();
+
+                OutputStream outputStream = new BufferedOutputStream(urlConnection.getOutputStream());
+                outputStream.flush(); //pushes the request through straight away.
+                outputStream.close();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if(buffer.length() == 0) {
+                    return null;
+                }
+                return buffer.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            text.setText(s);
+        }
     }
 }
